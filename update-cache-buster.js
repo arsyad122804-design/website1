6f51ed589105e19eeff82c09f2257724f4d25785
arsyad122804-design <arsyad122804-design@users.noreply.github.com>
2026-09-15 15:18:18 +0700

    chore: bump cache busters to v9 to force refresh 7 official YouTube Shorts

--- update-cache-buster.js
+++ update-cache-buster.js
@@ -7,11 +7,11 @@ const files = fs.readdirSync(dir).filter(f => f.endsWith('.html'));
 files.forEach(file => {
   const filePath = path.join(dir, file);
   let content = fs.readFileSync(filePath, 'utf8');
-  content = content.replace(/navbar\.js(\?v=[^"'\s>]*)?/g, 'navbar.js?v=20260915_v8');
-  content = content.replace(/style\.css(\?v=[^"'\s>]*)?/g, 'style.css?v=20260915_v8');
-  content = content.replace(/navbar\.css(\?v=[^"'\s>]*)?/g, 'navbar.css?v=20260915_v8');
-  content = content.replace(/mobile-nav\.js(\?v=[^"'\s>]*)?/g, 'mobile-nav.js?v=20260915_v8');
+  content = content.replace(/navbar\.js(\?v=[^"'\s>]*)?/g, 'navbar.js?v=20260915_v9');
+  content = content.replace(/style\.css(\?v=[^"'\s>]*)?/g, 'style.css?v=20260915_v9');
+  content = content.replace(/navbar\.css(\?v=[^"'\s>]*)?/g, 'navbar.css?v=20260915_v9');
+  content = content.replace(/media-sosial\.css(\?v=[^"'\s>]*)?/g, 'media-sosial.css?v=20260915_v9');
   fs.writeFileSync(filePath, content, 'utf8');
 });
 
-console.log('Updated cache busters v8 in all HTML files.');
+console.log('Updated cache busters v9 in all HTML files.');
