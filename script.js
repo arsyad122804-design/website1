158489cd37ad48936eaf0a3b4a58d8b6f73cac70
arsyad122804-design <arsyad122804-design@users.noreply.github.com>
2026-09-26 14:44:02 +0700

    style: tambahkan kapsul navbar dengan garis tepi (border) hitam tebal 2.5px

--- script.js
+++ script.js
@@ -192,9 +192,9 @@
     s.async = false;
     document.body.appendChild(s);
   }
-  load('mobile-nav.js?v=20260926_v211');
-  load('site-footer.js?v=20260926_v211');
-  load('visitor-tracker.js?v=20260926_v211');
+  load('mobile-nav.js?v=20260926_v212');
+  load('site-footer.js?v=20260926_v212');
+  load('visitor-tracker.js?v=20260926_v212');
 })();
 // ===========================
 // SEARCH MODAL FUNCTIONALITY
